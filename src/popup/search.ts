129d7a604862b63a89301d3d66d6ca000fd105d2
cary <812372447@qq.com>
2022-02-10 00:55:13 +0800

    fix: 解决动态评论无法点赞点踩的问题

--- src/popup/search.ts
+++ src/popup/search.ts
@@ -167,39 +167,49 @@ export const getReply = async () => {
 }
 
 export const ReplyAction = async (info: IMatchInfo) => {
-  const oid = await getOid()
-  const { action, like, rpid } = info
-  const csrf = /(?<=bili_jct=)\w+/.exec(document.cookie)?.[0]
-  if (!csrf) {
-    ElMessage.error('csrf不存在，可尝试重新登陆')
-    return
-  }
-  const { code, message } = await replyAction({ oid, rpid, action: action === EActionStatus.LIKE ? EStatus.CANCEL : EStatus.SURE, csrf })
-  if (code) {
-    ElMessage.error(message)
-  } else {
-    const action_flag = action === EActionStatus.LIKE
-    info.action = action_flag ? EActionStatus.STATELESS : EActionStatus.LIKE
-    info.like = action_flag ? like - 1 : like + 1
-    ElMessage.success(action_flag ? '取消点赞' : '点赞成功')
+  try {
+    const oid = await getOid()
+    const { action, like, rpid } = info
+    const { dyid } = filter.value
+    const csrf = /(?<=bili_jct=)\w+/.exec(document.cookie)?.[0]
+    if (!csrf) {
+      ElMessage.error('csrf不存在，可尝试重新登陆')
+      return
+    }
+    const { code, message } = await replyAction({ oid, type: dyid ? 11 : 1, rpid, action: action === EActionStatus.LIKE ? EStatus.CANCEL : EStatus.SURE, csrf })
+    if (code) {
+      ElMessage.error(message)
+    } else {
+      const action_flag = action === EActionStatus.LIKE
+      info.action = action_flag ? EActionStatus.STATELESS : EActionStatus.LIKE
+      info.like = action_flag ? like - 1 : like + 1
+      ElMessage.success(action_flag ? '取消点赞' : '点赞成功')
+    }
+  } catch (err) {
+    ElMessage.error((err as Error).message)
   }
 }
 
 export const ReplyHate = async (info: IMatchInfo) => {
-  const oid = await getOid()
-  const { action, rpid } = info
-  const csrf = /(?<=bili_jct=)\w+/.exec(document.cookie)?.[0]
-  if (!csrf) {
-    ElMessage.error('csrf不存在，可尝试重新登陆')
-    return
-  }
-  const { code, message } = await replyHate({ oid, rpid, action: action === EActionStatus.HATE ? EStatus.CANCEL : EStatus.SURE, csrf })
-  if (code) {
-    ElMessage.error(message)
-  } else {
-    const action_flag = action === EActionStatus.HATE
-    info.action = action_flag ? EActionStatus.STATELESS : EActionStatus.HATE
-    if (action === EActionStatus.LIKE && !action_flag) info.like--
-    ElMessage.success(action_flag ? '取消点踩' : '点踩成功')
+  try {
+    const oid = await getOid()
+    const { action, rpid } = info
+    const { dyid } = filter.value
+    const csrf = /(?<=bili_jct=)\w+/.exec(document.cookie)?.[0]
+    if (!csrf) {
+      ElMessage.error('csrf不存在，可尝试重新登陆')
+      return
+    }
+    const { code, message } = await replyHate({ oid, type: dyid ? 11 : 1, rpid, action: action === EActionStatus.HATE ? EStatus.CANCEL : EStatus.SURE, csrf })
+    if (code) {
+      ElMessage.error(message)
+    } else {
+      const action_flag = action === EActionStatus.HATE
+      info.action = action_flag ? EActionStatus.STATELESS : EActionStatus.HATE
+      if (action === EActionStatus.LIKE && !action_flag) info.like--
+      ElMessage.success(action_flag ? '取消点踩' : '点踩成功')
+    }
+  } catch (err) {
+    ElMessage.error((err as Error).message)
   }
 }
